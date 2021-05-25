37c2fa1d593d6c5f520a5ad2f5d0b55bc21d55cb
SimoneBressan <simone@kuama.it>
2021-05-25 17:23:17 +0200

    Add Entity Improved UseCase

--- lib/kuama_dart_domain.dart
+++ lib/kuama_dart_domain.dart
@@ -3,15 +3,16 @@
 /// More dartdocs go here.
 library kuama_dart_domain;
 
+export 'package:built_collection/built_collection.dart';
 export 'package:dartz/dartz.dart' hide State, StateT, StateMonad, StateTMonad;
 export 'package:get_it/get_it.dart';
 export 'package:logging/logging.dart';
 export 'package:rxdart/rxdart.dart';
 
+export 'src/shared/entities/entity.dart';
 export 'src/shared/errors/failure.dart';
 export 'src/shared/errors/fault.dart';
 export 'src/shared/logger/loggers.dart';
-export 'src/shared/pretty_formatter/logger_interceptor.dart';
 export 'src/shared/pretty_formatter/pretty_formatter.dart';
 export 'src/shared/repository/repository.dart';
 export 'src/shared/use_case/use_case.dart';
