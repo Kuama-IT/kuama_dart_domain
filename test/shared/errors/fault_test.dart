4ed9fc32f7eab0e88cae6c9b7c38c23e55314068
SimoneBressan <simone@kuama.it>
2021-06-08 12:12:11 +0200

    - Added support for HttpClient and Platform type faults - Added support for HttpClient and Platform failures - Improved the readability of faults and failures, they are now printed starting with the innermost error

---
+++ test/shared/errors/fault_test.dart
@@ -0,0 +1,32 @@
+import 'package:kuama_dart_domain/src/shared/errors/fault.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+
+void main() {
+  group('Test Fault', () {
+    test('Test prettyMap() method work', () {
+      final fault = HttpClientFault();
+
+      expect(fault.toPrettyMap(), isNotEmpty);
+    });
+
+    test('Test toString() method work', () {
+      final fault = HttpClientFault();
+
+      expect(fault.toString(), isNotEmpty);
+    });
+
+    test('Test prettyMap() method work with generic error', () {
+      final fault = HttpClientFault(error: ErrorAndStackTrace('Error', StackTrace.current));
+
+      expect(fault.toPrettyMap(), isNotEmpty);
+    });
+
+    test('Test toString() method work with generic error', () {
+      final fault = HttpClientFault(error: ErrorAndStackTrace('Error', StackTrace.current));
+
+      expect(fault.toString(), isNotEmpty);
+    });
+  });
+}
