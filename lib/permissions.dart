a1fc53bf3001126bfefc02ec1e097a93171a5c52
SimoneBressan <simone@kuama.it>
2021-06-16 12:56:24 +0200

    Renamed some files Added support for saving permissions information in preferences

--- lib/permissions.dart
+++ lib/permissions.dart
@@ -1,8 +1,9 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-library kuama_dart_feature_permissions;
+library kuama_dart_permissions;
 
 export 'src/features/permissions/entities/permission.dart';
+export 'src/features/permissions/repositories/permission_preferences_repository.dart';
 export 'src/features/permissions/repositories/permission_repository.dart';
 export 'src/features/permissions/use_cases/can_ask_permission.dart';
 export 'src/features/permissions/use_cases/check_permission.dart';
--- lib/src/features/permissions/use_cases/update_can_ask_permission.dart
+++ lib/src/features/permissions/use_cases/update_can_ask_permission.dart
@@ -1,12 +1,30 @@
+import 'package:dartz/dartz.dart';
+import 'package:get_it/get_it.dart';
 import 'package:kuama_dart_domain/src/features/permissions/entities/permission.dart';
+import 'package:kuama_dart_domain/src/features/permissions/repositories/permission_preferences_repository.dart';
+import 'package:kuama_dart_domain/src/shared/errors/failure.dart';
 import 'package:kuama_dart_domain/src/shared/use_case/use_case.dart';
 
 class UpdateCanAskPermissionParams extends ParamsBase {
   final Permission permission;
-  final bool canRequire;
+  final bool canAsk;
 
-  UpdateCanAskPermissionParams(this.permission, this.canRequire) : super([permission, canRequire]);
+  UpdateCanAskPermissionParams(this.permission, this.canAsk) : super([permission, canAsk]);
 }
 
 /// Update if the permit can be requested
-abstract class UpdateCanAskPermission extends UseCase<UpdateCanAskPermissionParams, bool> {}
+abstract class UpdateCanAskPermission extends UseCase<UpdateCanAskPermissionParams, bool> {
+  UpdateCanAskPermission();
+
+  factory UpdateCanAskPermission.factory() = _PreferencesUpdateCanAskPermission;
+}
+
+/// [UpdateCanAskPermission] Use the repository [PermissionPreferencesRepository]
+class _PreferencesUpdateCanAskPermission extends UpdateCanAskPermission {
+  final PermissionPreferencesRepository prefRepo = GetIt.I();
+
+  @override
+  Stream<Either<Failure, bool>> tryCall(UpdateCanAskPermissionParams params) async* {
+    yield* prefRepo.setCanAsk(params.permission, params.canAsk).toRight();
+  }
+}
